3d8fa6dcc653e3cc77587473cf2d4e15a3168ecf
Raluca Elisabeta Rusu <raluca.elisabeta.rusu@gmail.com>
2024-08-09 18:18:40 +0200

    getUserBalance action

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,8 +1,11 @@
-const Balance = () => {
+import getUserBalance from '@/app/actions/getUserBalance';
+
+const Balance = async () => {
+  const { balance } = await getUserBalance();
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$500</h1>
+      <h1>${balance}</h1>
     </>
   );
 };
